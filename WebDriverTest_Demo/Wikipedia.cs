7d5a3a763d213ebce6844a3d162fa0942657b63d
aysel <aysel@ICEBABY>
2022-05-18 16:43:05 +0300

    Add project files.

---
+++ WebDriverTest_Demo/Wikipedia.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace WebDriverTest_Demo
+{
+    internal class Wikipedia
+    {
+        static void Main(string[] args)
+        {
+            
+            var driver = new ChromeDriver();
+
+           
+            driver.Url = "https://wikipedia.org";
+
+            System.Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+           
+            var searchField = driver.FindElement(By.Id("searchInput"));
+
+         
+            searchField.Click();
+
+            
+            searchField.SendKeys("BURGAS" + Keys.Enter);
+
+
+            System.Console.WriteLine("TITLE AFTER SEARCH: " + driver.Title);
+
+            
+            driver.Quit();
+
+
+
+
+        }
+    }
+}
